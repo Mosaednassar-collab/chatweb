fbc89f5bd91d9aa21d61f0984b5ea8034012df7b
Mosaednassar-collab <Mosaed.nassar@outlook.sa>
2026-05-01 05:19:41 +0300

    Initialize Express server with Socket.io support

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    socket.on("joinRoom", (room) => {
+        socket.join(room);
+    });
+
+    socket.on("chatMessage", (data) => {
+        io.to(data.room).emit("chatMessage", data.message);
+    });
+});
+
+http.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
